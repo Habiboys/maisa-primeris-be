35939eb8bfee99a9ddcb6566445fdda8fa86f99c
Habiboys <nouvalhabibie18@gmail.com>
2026-04-01 13:16:48 +0700

    oke

--- src/app.js
+++ src/app.js
@@ -31,7 +31,6 @@ app.use((req, res, next) => {
 });
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(captureResponseBody);
